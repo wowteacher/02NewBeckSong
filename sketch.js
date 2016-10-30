cb053a595d5eaa664f82bb4b5bc8c132903384ba
wowteacher <francesca.sampietro@gmail.com>
2016-10-30 12:09:57 +0100

    Aggiunto pulsante
    Pulsante Genera canzone

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 var input;
+var button;
 
 
 function setup() {
 	noCanvas();
 	input = createInput('In the time of chimpanzees I was a monkey Butane in my veins');
+	button = createButton ('Genera canzone');
 	input.changed(processRita);
+	button.mousePressed(processRita);
 	input.size(400);
 
 }
